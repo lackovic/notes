bdc28c5843d1d6f52d922f4c8b6e80163a6277f9
Marco Lackovic <marco.lackovic@gmail.com>
2020-03-14 19:00:21 +0200

    Add PowerShell commands to import Chocolatey profile

---
+++ Windows/Chocolatey/Profile.ps1
@@ -0,0 +1,6 @@
+# Include it in your PowerShell profile to publish Chocolatey packages
+
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
