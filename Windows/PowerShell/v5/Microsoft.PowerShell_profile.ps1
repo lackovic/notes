943799f294719ff360ad9a17258479e7c60d5e4b
Marco Lackovic <marco.lackovic@gmail.com>
2025-01-19 22:36:44 +0200

    Add back posh-git as prompt renderer

--- Windows/PowerShell/v5/Microsoft.PowerShell_profile.ps1
+++ Windows/PowerShell/v5/Microsoft.PowerShell_profile.ps1
@@ -10,18 +10,29 @@ Import-Module PSColor
 $global:PSColor.File.Code.Pattern = '\.(java|c|cpp|cs|js|jsx|ts|tsx|css|html)$'
 $global:PSColor.File.Text.Pattern = '\.(txt|cfg|conf|ini|csv|log|config|xml|yml|md|markdown|json)$'
 
-# Use Oh My Posh as prompt renderer https://github.com/JanDeDobbeleer/oh-my-posh
+# Prompt renderers
+#
+# Option 1: https://github.com/JanDeDobbeleer/oh-my-posh
 # 1. winget install --id JanDeDobbeleer.OhMyPosh
 # 2. oh-my-posh font install Inconsolata
 # 3. To enable language versions in your theme see https://github.com/JanDeDobbeleer/oh-my-posh/discussions/5902#discussioncomment-11284862
-oh-my-posh --init --shell pwsh --config ~/AppData/Local/Programs/oh-my-posh/themes/powerlevel10k_rainbow.omp.json | Invoke-Expression
+# 4. Uncomment the following line to enable Oh My Posh:
+#oh-my-posh --init --shell pwsh --config ~/AppData/Local/Programs/oh-my-posh/themes/powerlevel10k_rainbow.omp.json | Invoke-Expression
+
+# Option 2: https://github.com/dahlbyk/posh-git
+# 1. Run as admin "PowerShellGet\Install-Module posh-git -Scope CurrentUser -Force"
+# 2. Uncomment the following lines to enable posh-git:
+#Import-Module posh-git
+# 3. Uncomment the following lines to prefix each prompt with date and time:
+#$GitPromptSettings.DefaultPromptPrefix.Text = '$(Get-Date -f "HH:mm:ss") '
+#$GitPromptSettings.DefaultPromptPrefix.ForegroundColor = [ConsoleColor]::Magenta
 
 # See system information win Winfetch
 # Run as admin "Install-Script -Name pwshfetch-test-1"
 Set-Alias winfetch pwshfetch-test-1
 
-# Set default folder
-Set-Location C:\src
+# To set the default starting folder uncomment the following line:
+#Set-Location C:\src
 
 # Setup to use fnm https://github.com/Schniz/fnm#shell-setup
 fnm env --use-on-cd | Out-String | Invoke-Expression
