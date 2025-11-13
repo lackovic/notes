0c6f460469c812cf46db8a031506778a1a65cf1b
Marco Lackovic <marco.lackovic@gmail.com>
2025-11-13 08:30:11 +0200

    Add initial setup Windows app installation script

---
+++ Windows/SetupApps.ps1
@@ -0,0 +1,23 @@
+winget source update
+
+# General Apps
+winget install chrome
+winget install vscode
+winget install -h --id Git.Git
+winget install -h --id DominikReichl.KeePass
+winget install -h --id FastStone.Viewer
+winget install -h --id AutoHotkey.AutoHotkey
+winget install -h --id Microsoft.PowerShell
+winget install -h --id LocalSend.LocalSend
+
+# Work Apps
+winget install -h --id JetBrains.IntelliJIDEA.Ultimate
+winget install -h --id SlackTechnologies.Slack
+winget install -h --id DBeaver.DBeaver.Community
+winget install -h --id Docker.DockerDesktop
+winget install -h --id Schniz.fnm
+winget install -h --id Bruno.Bruno
+winget install -h --id Gravitational.Teleport
+
+# Windows Subsystem for Linux
+wsl --install
