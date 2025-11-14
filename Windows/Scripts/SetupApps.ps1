0646123acb84517c1d3b17e6fe2e80edf385a69c
Marco Lackovic <marco.lackovic@gmail.com>
2025-11-14 23:44:55 +0200

    Add more apps to the Windows setup script

--- Windows/Scripts/SetupApps.ps1
+++ Windows/Scripts/SetupApps.ps1
@@ -10,15 +10,32 @@
 winget source update
 
 # General Apps
-winget install chrome
-winget install vscode
-winget install -h --id Git.Git
+winget install -h --id Google.Chrome
+winget install -h --id Telegram.TelegramDesktop
+winget install -h --id Mega.MEGASync
 winget install -h --id DominikReichl.KeePass
-winget install -h --id FastStone.Viewer
 winget install -h --id AutoHotkey.AutoHotkey
 winget install -h --id Microsoft.PowerShell
+winget install -h --id HermannSchinagl.LinkShellExtension
+winget install -h --id Microsoft.VisualStudioCode
+winget install -h --id Git.Git
+winget install -h --id FastStone.Viewer
 winget install -h --id LocalSend.LocalSend
-winget install -h --id Mega.MEGASync
+winget install -h --id TGRMNSoftware.BulkRenameUtility
+winget install -h --id SumatraPDF.SumatraPDF
+winget install -h --id Spotify.Spotify
+winget install -h --id GIMP.GIMP.3
+winget install -h --id RIA.eIDsoftware
+winget install -h --id CodecGuide.K-LiteCodecPack.Mega
+winget install -h --id Avidemux.Avidemux
+winget install -h --id JAMSoftware.TreeSize.Frees
+
+# Gaming Apps
+winget install -h --id Discord.Discord
+winget install -h --id Valve.Steam
+winget install -h --id Amazon.Games
+winget install -h --id GOG.Galaxy
+winget install -h --id EpicGames.EpicGamesLauncher
 
 # Work Apps
 winget install -h --id JetBrains.IntelliJIDEA.Ultimate
