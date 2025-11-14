3f6911072df54e842ca99b0a9963d5e771f88c56
Marco Lackovic <marco.lackovic@gmail.com>
2025-11-14 15:10:01 +0200

    Group Explorer settings in Windows configuration setup script

--- Windows/Scripts/SetupConfig.ps1
+++ Windows/Scripts/SetupConfig.ps1
@@ -21,7 +21,21 @@ Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer
 # Set Taskbar alignment to Left (0 = Left, 1 = Center)
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarAl" -Type DWord -Value 0
 
-# <<< Taskbar Settings <<<
+# >>> Explorer Settings >>>
+
+# Hide desktop icons
+Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name HideIcons -Value 1
+
+# Enable showing hidden files and file extensions in Windows Explorer
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "HideFileExt" -Value 0
+
+# Enable classic context menu
+reg add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
+
+# Restart Explorer to apply the above changes
+Stop-Process -Name explorer -Force; Start-Process explorer
+
+# >>> Other Settings >>>
 
 # Enable System > Notifications > Do not disturb
 # ?
@@ -36,22 +50,10 @@ Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\P
 # https://github.com/Raphire/Win11Debloat/issues/120
 # Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name "UserPreferencesMask" -Value ([byte[]](0x90,0x12,0x07,0x80,0x10,0x00,0x00,0x00)) -Type Binary
 
-# Hide desktop icons
-Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name HideIcons -Value 1
-
 # Disable Cortana and web search integration in Windows Search
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Search" /v BingSearchEnabled /t REG_DWORD /d 00000000 /f
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Search" /v AllowSearchToUseLocation /t REG_DWORD /d 00000000 /f
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Search" /v CortanaConsent /t REG_DWORD /d 00000000 /f
-
-# Enable classic context menu
-reg add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
-
-# Enable showing hidden files and file extensions in Windows Explorer
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "HideFileExt" -Value 0
-
-# Restart Explorer to apply the above changes
-Stop-Process -Name explorer -Force; Start-Process explorer
 
 # Disable notifications
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\PushNotifications" -Name "ToastEnabled" -Type DWord -Value 0 -Force
